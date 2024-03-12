09b0b2b20beb054f0c82d6d965f9a0b07dd41ad0
Sandi Perdiansah <sandiperdiansah6@gmail.com>
2024-03-13 06:36:54 +0700

    commit kedua

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 const data = [
-  { id: 1, image: '../assets/img/gallery/1.jpg' },
-  { id: 2, image: '../assets/img/gallery/2.jpg' },
-  { id: 3, image: '../assets/img/gallery/3.jpg' },
-  { id: 4, image: '../assets/img/gallery/4.jpg' },
-  { id: 5, image: '../assets/img/gallery/5.jpg' },
-  { id: 6, image: '../assets/img/gallery/6.jpg' },
-  { id: 7, image: '../assets/img/gallery/7.jpg' },
-  { id: 8, image: '../assets/img/gallery/8.jpg' },
+  { id: 1, image: './assets/img/gallery/1.jpg' },
+  { id: 2, image: './assets/img/gallery/2.jpg' },
+  { id: 3, image: './assets/img/gallery/3.jpg' },
+  { id: 4, image: './assets/img/gallery/4.jpg' },
+  { id: 5, image: './assets/img/gallery/5.jpg' },
+  { id: 6, image: './assets/img/gallery/6.jpg' },
+  { id: 7, image: './assets/img/gallery/7.jpg' },
+  { id: 8, image: './assets/img/gallery/8.jpg' },
 ];
 
 const swiperImageSlider = ((dataSwipperImage) => {
