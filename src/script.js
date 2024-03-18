66f27a20bf41ee5bde138648a4bfe4524f8bbcce
Sandi Perdiansah <sandiperdiansah6@gmail.com>
2024-03-19 01:24:27 +0700

    menambahkan comentar

--- src/script.js
+++ src/script.js
@@ -69,5 +69,16 @@ simplyCountdown('#simply-countdown', {
   },
 });
 
+// disqus
+(function () {
+  // DON'T EDIT BELOW THIS LINE
+  var d = document,
+    s = d.createElement('script');
+  s.src = 'https://nengdepi-aayogi.disqus.com/embed.js';
+  s.setAttribute('data-timestamp', +new Date());
+  (d.head || d.body).appendChild(s);
+})();
+
+
 // animate on scroll
 AOS.init();
